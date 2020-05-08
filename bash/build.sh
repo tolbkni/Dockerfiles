8d2ebeec829a84f43030cab2aea5bc3803635efe
Gao Jiangmiao <tolbkni@gmail.com>
2020-05-08 09:27:31 +0800

    [UPD] alpine version bump to 3.11

--- bash/build.sh
+++ bash/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t tolbkni/alpine:3.10 -t tolbkni/alpine:latest .
+docker build -t tolbkni/alpine:3.11 -t tolbkni/alpine:latest .
 
-docker push tolbkni/alpine:3.10
+docker push tolbkni/alpine:3.11
 docker push tolbkni/alpine:latest
